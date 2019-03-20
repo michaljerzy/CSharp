386e0104b4384db646a299e5ae533c3791be5c36
michaljerzy <michal.jerzy99@gmail.com>
2019-03-20 14:35:26 +0100

    20.03.2019 dziwne rzeczy, nie rozumiem

---
+++ Andrzejewski_20_03_2019/Andrzejewski_20_03_2019/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Andrzejewski_20_03_2019
+{
+    partial class Andrzejewski
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblTxtn = new System.Windows.Forms.Label();
+            this.txtN = new System.Windows.Forms.TextBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.lblwybierzfigurę = new System.Windows.Forms.Label();
+            this.panelRysownica = new System.Windows.Forms.Panel();
+            this.butStart = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTxtn
+            // 
+            this.lblTxtn.AutoSize = true;
+            this.lblTxtn.Location = new System.Drawing.Point(0, 0);
+            this.lblTxtn.Name = "lblTxtn";
+            this.lblTxtn.Size = new System.Drawing.Size(167, 13);
+            this.lblTxtn.TabIndex = 0;
+            this.lblTxtn.Text = "Podaj liczbę figur geometrycznych";
+            // 
+            // txtN
+            // 
+            this.txtN.Location = new System.Drawing.Point(30, 16);
+            this.txtN.Name = "txtN";
+            this.txtN.Size = new System.Drawing.Size(100, 20);
+            this.txtN.TabIndex = 2;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Punkt",
+            "Linia",
+            "Elipsa",
+            "Okrąg",
+            "Prostokąt",
+            "Kwadrat",
+            "Wielokąt foremny",
+            "Koło"});
+            this.checkedListBox1.Location = new System.Drawing.Point(655, 62);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 124);
+            this.checkedListBox1.TabIndex = 3;
+            // 
+            // lblwybierzfigurę
+            // 
+            this.lblwybierzfigurę.AutoSize = true;
+            this.lblwybierzfigurę.Location = new System.Drawing.Point(645, 37);
+            this.lblwybierzfigurę.Name = "lblwybierzfigurę";
+            this.lblwybierzfigurę.Size = new System.Drawing.Size(143, 13);
+            this.lblwybierzfigurę.TabIndex = 5;
+            this.lblwybierzfigurę.Text = "Wybierz figurę do prezentacji";
+            // 
+            // panelRysownica
+            // 
+            this.panelRysownica.Location = new System.Drawing.Point(97, 69);
+            this.panelRysownica.Name = "panelRysownica";
+            this.panelRysownica.Size = new System.Drawing.Size(487, 283);
+            this.panelRysownica.TabIndex = 6;
+            // 
+            // butStart
+            // 
+            this.butStart.Location = new System.Drawing.Point(655, 192);
+            this.butStart.Name = "butStart";
+            this.butStart.Size = new System.Drawing.Size(120, 51);
+            this.butStart.TabIndex = 7;
+            this.butStart.Text = "START";
+            this.butStart.UseVisualStyleBackColor = true;
+            this.butStart.Click += new System.EventHandler(this.butStart_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Andrzejewski
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.butStart);
+            this.Controls.Add(this.panelRysownica);
+            this.Controls.Add(this.lblwybierzfigurę);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.txtN);
+            this.Controls.Add(this.lblTxtn);
+            this.Name = "Andrzejewski";
+            this.Text = "Prezentacja wybranych figur geometrycznych";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTxtn;
+        private System.Windows.Forms.TextBox txtN;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label lblwybierzfigurę;
+        private System.Windows.Forms.Panel panelRysownica;
+        private System.Windows.Forms.Button butStart;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ Andrzejewski_20_03_2019/Andrzejewski_20_03_2019/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//dodanie przestrzeni nazw 2D
+using System.Drawing.Drawing2D;
+
+namespace Andrzejewski_20_03_2019
+{
+    public partial class Andrzejewski : Form
+    {// deklaracja rysownicy
+        Graphics Rysownica;
+        Punkt[] TablFigGeom;
+        int IndexFigur;
+        //deklaracje stałych
+        const int MarginesPanel = 10;
+        const int MarginesFormularza = 20;
+
+        public Andrzejewski()
+        {
+            InitializeComponent();
+            //lokalizacja i zwymiarowanie formularza
+            this.Left = MarginesFormularza;
+            this.Top = MarginesFormularza;
+            this.Width = (int)(Width * 1.1F);
+            this.Height = (int)(Height * 0.9F);
+            this.StartPosition = FormStartPosition.Manual;
+            //loklizacja i zwymiarowanie kontrolki panel
+            panelRysownica.Location = new Point(txtN.Location.X + txtN.Width + 50, 3 * MarginesFormularza);
+            panelRysownica.Width = (int)(this.Width * 0.6F);
+            panelRysownica.Height = (int)(this.Height * 0.7F);
+            panelRysownica.BackColor = Color.Beige;
+            panelRysownica.BorderStyle = BorderStyle.Fixed3D;
+            //lokalizacha kontrolki wyboru figur geometrycznych
+            checkedListBox1.Location = new Point(panelRysownica.Location.X + panelRysownica.Width + MarginesFormularza,
+                panelRysownica.Location.Y);
+            lblwybierzfigurę.Location = new Point(checkedListBox1.Location.X, (int)(checkedListBox1.Location.Y * 0.1F));
+            butStart.Location = new Point(checkedListBox1.Location.X, (int)(checkedListBox1.Location.Y - 32));
+            //utworzenie egzemplarza powierzchni graficznej
+            Rysownica = panelRysownica.CreateGraphics();
+        }
+        class Punkt
+        {
+            const int PromieńPuntu = 5;
+            int RozmiarPunktu;
+            // deklaracja atrybutów, które mają być dostępne dla klas pochodnycych
+            protected int X;
+            protected int Y;
+            protected Color Kolor;
+            protected bool Widoczny;
+            //atrybuty dla klas pochodnych 
+            protected DashStyle StylLinii;
+            protected int GrubośćLinii;
+            //deklaracje konstruktorow 
+            public Punkt(int x, int y)
+            {
+                X = x;
+                Y = y;
+                //ustawienia domyślne
+                RozmiarPunktu = 2 * PromieńPuntu;
+                Kolor = Color.Black;
+                GrubośćLinii = 1;
+                StylLinii = DashStyle.Solid;
+                Widoczny = false;
+
+            }
+            public Punkt(int X, int Y, Color Kolor)
+            {
+                //autoreferencja
+                this.X = X;
+                this.Y = Y;
+                this.Kolor = Kolor;
+                //ustawienia domyślne
+                RozmiarPunktu = 2 * PromieńPuntu;
+                Kolor = Color.Black;
+                GrubośćLinii = 1;
+                StylLinii = DashStyle.Solid;
+                Widoczny = false;
+            }
+            //deklarache metrod prywatnych
+            private void UstawXY(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            private void UstawXY(Point NowaLokalizacja)
+            {
+                X = NowaLokalizacja.X;
+                Y = NowaLokalizacja.Y;
+            }
+            //deklaracja metod publicznych przycisków dla obsługi przycisków funkcjonalnych programu
+            public void PrzesuńDoNowegoXY(Control Kontrolka, Graphics PowierzchniaGraficzna, int x, int y)
+            {
+                Wymaz(Kontrolka, PowierzchniaGraficzna);
+                UstawXY(x, y);
+                Wykresl(PowierzchniaGraficzna);
+            }
+            public void FormatujFG(Color Kolor, DashStyle StylLinii, int GruboscLinii)
+            {
+                this.Kolor = Kolor;
+                this.StylLinii = StylLinii;
+                this.GrubośćLinii = GrubośćLinii;
+            }
+            //deklaracja metod wirtualnych
+            public virtual void Wykresl(Graphics PowierzchniaGraficzna)
+            {
+                //wykreślenie (wymalowanie) punktu
+                SolidBrush Pedzel = new SolidBrush(Kolor);
+                PowierzchniaGraficzna.FillEllipse(Pedzel, X - RozmiarPunktu / 2, Y = RozmiarPunktu / 2, RozmiarPunktu, RozmiarPunktu);
+                //ustawienie atrybutu widoczności
+                Widoczny = true;
+                Pedzel.Dispose();
+
+            }
+
+            public virtual void Wymaz(Control Kontrolka, Graphics PowierzchniaGraficzna)
+            {
+                if (Widoczny)
+                {
+                    SolidBrush Pedzel = new SolidBrush(Kontrolka.BackColor);
+                    PowierzchniaGraficzna.FillEllipse(Pedzel, X - RozmiarPunktu / 2, Y = RozmiarPunktu / 2, RozmiarPunktu, RozmiarPunktu);
+                    //ustawienie atrybutu widoczności
+                    Widoczny = false;
+                    Pedzel.Dispose();
+                }
+            }
+        }//do klasy punkt
+        class Linia : Punkt
+        {
+            //deklaracja atrybutu geometrycznego końca linii
+            int Xk, Yk;
+            //deklaracje kontruktorów
+            public Linia(int Xp, int Yp, int Xk, int Yk):base (Xp, Yp)
+            {
+                this.Xk = Xk;
+                this.Yk = Yk;
+            }
+
+            public Linia(int Xp, int Yp, int Xk, int Yk, Color Kolor, DashStyle StylLinii, int GruboscLinii) : base(Xp, Yp, Kolor)
+            {
+                this.Xk = Xk;
+                this.Yk = Yk;
+                this.StylLinii = StylLinii;
+                this.GrubośćLinii = GrubośćLinii;
+            }
+            //nadpisanie metod wirtualnych
+            public override void Wykresl(Graphics PowierzchniaGraficzna)
+            {
+                Pen Pioro = new Pen(Kolor, GrubośćLinii);
+                Pioro.DashStyle = StylLinii;
+                //wykreślenie linii
+                PowierzchniaGraficzna.DrawLine(Pioro, X, Y, Xk, Yk);
+                //utworzenie atrybutów widoczności
+                Widoczny = true;
+                Pioro.Dispose();
+            }
+            public override void Wymaz(Control Kontrolka, Graphics PowierzchniaGraficzna)
+            {
+                if (Widoczny)
+                {
+                    Pen Pioro = new Pen(Kontrolka.BackColor, GrubośćLinii);
+                    Pioro.DashStyle = StylLinii;
+                    //wykreślenie linii
+                    PowierzchniaGraficzna.DrawLine(Pioro, X, Y, Xk, Yk);
+                    //utworzenie atrybutów widoczności
+                    Widoczny = true;
+                    Pioro.Dispose();
+                }
+            }
+        }// od klasy linia
+        class Elipsa : Punkt
+        {
+            protected int OśDuża;
+            protected int OśMała;
+            // deklaracje konstruktorów
+            public Elipsa(int x, int y, int OśDuża, int OśMała, Color Kolor, DashStyle StylLinii, int GrubośćLinii): base(x, y, Kolor)
+            {
+                this.OśDuża = OśDuża;
+                this.OśMała = OśMała;
+                this.StylLinii = StylLinii;
+                this.GrubośćLinii = GrubośćLinii;
+            }
+            //nadpisanie metod wirtualnych
+            public override void Wykresl(Graphics PowierzchniaGraficzna)
+            {
+                Pen Pioro = new Pen(Kolor, GrubośćLinii);
+                Pioro.DashStyle = StylLinii;
+                //wykreślenie linii
+                PowierzchniaGraficzna.DrawEllipse(Pioro, X - OśDuża/2, Y - OśMała/2, OśDuża, OśMała);
+                //utworzenie atrybutów widoczności
+                Widoczny = true;
+                Pioro.Dispose();
+            }
+
+            public override void Wymaz(Control Kontrolka, Graphics PowierzchniaGraficzna)
+            {
+                if (Widoczny)
+                {
+                    Pen Pioro = new Pen(Kontrolka.BackColor, GrubośćLinii);
+                    Pioro.DashStyle = StylLinii;
+                    //wykreślenie linii
+                    PowierzchniaGraficzna.DrawEllipse(Pioro, X - OśDuża / 2, Y - OśMała / 2, OśDuża, OśMała);
+                    //utworzenie atrybutów widoczności
+                    Widoczny = false;
+                    Pioro.Dispose();
+                }
+            }
+        }//od klasy ellipsa 
+
+        private void butStart_Click(object sender, EventArgs e)
+        {
+            int N; //ilośc figur
+            //pobranie ze sprawdzeniem liczby figur geometrycznych do prezentacji
+            if (string.IsNullOrEmpty(txtN.Text.Trim()))
+            {
+                // zapalenie errorprovider
+                errorProvider1.SetError(txtN, "ERROR: musisz podać liczbę figur geometrycznych ");
+                return;
+            }
+            else
+                errorProvider1.Dispose();
+            //pobranie liczby figur
+            if(!int.TryParse(txtN.Text, out N))
+            {
+                // zapalenie errorprovider
+                errorProvider1.SetError(txtN, "ERROR: Wystąpił niedozwolony znak w zapisie liczby figur");
+                return;
+            }
+            else 
+                //sprawdzenie warunku wejściowego na N: N > 0
+                if (N <= 0)
+            {
+                // zapalenie errorprovider
+                errorProvider1.SetError(txtN, "ERROR: musisz podać liczbę wiekszą od zera ");
+                return;
+            }
+            //utworzenie egzemplarza tablicy "ewidencji" figur geometrycznych 
+            TablFigGeom = new Punkt[N];
+            // ustawienie indeksu dla tablicy TablFigGeom
+            IndexFigur = 0;
+            //ustawienie braku aktywności dla kontrolki 
+            txtN.Enabled = false;
+        }
+    }
+}
